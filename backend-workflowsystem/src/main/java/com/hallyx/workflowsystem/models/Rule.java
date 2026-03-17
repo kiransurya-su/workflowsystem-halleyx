8ab2712c570f32355838ae8099fed737a8dea68e
Kiran Surya <inbox.kiran123@gmail.com>
2026-03-18 02:04:57 +0530

    Fix: added missing JsonProperty import in Rule model

--- backend-workflowsystem/src/main/java/com/hallyx/workflowsystem/models/Rule.java
+++ backend-workflowsystem/src/main/java/com/hallyx/workflowsystem/models/Rule.java
@@ -2,6 +2,7 @@ package com.hallyx.workflowsystem.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
